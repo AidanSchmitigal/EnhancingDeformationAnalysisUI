d52d603d223c7b39178ac641681e21d4c82f0185
ajh416 <henryada@oregonstate.edu>
2025-02-18 16:03:48 -0800

    fix formatting in utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,50 +13,50 @@
 namespace utils {
 	std::string OpenFileDialog(const char* open_path, const bool folders_only, const char* filter) {
 #ifdef _WIN32
-			CoInitialize(nullptr);
-			IFileDialog* pFileDialog = nullptr;
-			std::wstring folderPath;
+		CoInitialize(nullptr);
+		IFileDialog* pFileDialog = nullptr;
+		std::wstring folderPath;
 
-			if (SUCCEEDED(CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL, IID_IFileDialog, reinterpret_cast<void**>(&pFileDialog))))
+		if (SUCCEEDED(CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL, IID_IFileDialog, reinterpret_cast<void**>(&pFileDialog))))
+		{
+			DWORD dwOptions;
+			if (SUCCEEDED(pFileDialog->GetOptions(&dwOptions)))
 			{
-					DWORD dwOptions;
-					if (SUCCEEDED(pFileDialog->GetOptions(&dwOptions)))
-					{
-							pFileDialog->SetOptions(dwOptions | FOS_PICKFOLDERS);
-					}
-
-					if (SUCCEEDED(pFileDialog->Show(nullptr)))
-					{
-							IShellItem* pItem;
-							if (SUCCEEDED(pFileDialog->GetResult(&pItem)))
-							{
-									PWSTR pszFilePath;
-									if (SUCCEEDED(pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath)))
-									{
-											folderPath = pszFilePath;
-											CoTaskMemFree(pszFilePath);
-									}
-									pItem->Release();
-							}
-					}
-					pFileDialog->Release();
+				pFileDialog->SetOptions(dwOptions | FOS_PICKFOLDERS);
 			}
 
-			CoUninitialize();
-			if (folderPath.empty()) return std::string();
+			if (SUCCEEDED(pFileDialog->Show(nullptr)))
+			{
+				IShellItem* pItem;
+				if (SUCCEEDED(pFileDialog->GetResult(&pItem)))
+				{
+					PWSTR pszFilePath;
+					if (SUCCEEDED(pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath)))
+					{
+						folderPath = pszFilePath;
+						CoTaskMemFree(pszFilePath);
+					}
+					pItem->Release();
+				}
+			}
+			pFileDialog->Release();
+		}
 
-			int size_needed = WideCharToMultiByte(CP_UTF8, 0, folderPath.c_str(), -1, nullptr, 0, nullptr, nullptr);
-			std::string str(size_needed - 1, 0);
-			WideCharToMultiByte(CP_UTF8, 0, folderPath.c_str(), -1, &str[0], size_needed, nullptr, nullptr);
-			return str;
+		CoUninitialize();
+		if (folderPath.empty()) return std::string();
+
+		int size_needed = WideCharToMultiByte(CP_UTF8, 0, folderPath.c_str(), -1, nullptr, 0, nullptr, nullptr);
+		std::string str(size_needed - 1, 0);
+		WideCharToMultiByte(CP_UTF8, 0, folderPath.c_str(), -1, &str[0], size_needed, nullptr, nullptr);
+		return str;
 #else
 
 		// Set zenity to open in our current directory
 		char buf[256];
 		if (folders_only)
-			snprintf(buf, 256, "zenity --file-selection --title=\"Choose an Image Folder\" --directory --filename=%s/", open_path);
+			snprintf(buf, 256, "zenity --file-selection --title=\"Choose an Image Folder (.tif)\" --directory --filename=%s/", open_path);
 		else
-			snprintf(buf, 256, "zenity --file-selection --title=\"Choose an Image Folder\" --filename=%s/", open_path);
+			snprintf(buf, 256, "zenity --file-selection --title=\"Choose an Image (.tif)\" --filename=%s/", open_path);
 
 		char output[1024];
 
@@ -99,7 +99,6 @@ namespace utils {
 		raster = (uint32_t*)_TIFFmalloc(npixels * sizeof(uint32_t));
 		if (raster) {
 			if (TIFFRGBAImageGet(&img, raster, width, height)) {
-				printf("Loaded %s, %d x %d\n", path, width, height);
 				// flip the image
 				uint32_t* temp = (uint32_t*)_TIFFmalloc(npixels * sizeof(uint32_t));
 				for (int i = 0; i < height; i++) {
