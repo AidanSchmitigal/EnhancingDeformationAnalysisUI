0c7b903d106e189b95116837c5e05d507b1af974
ajh416 <henryada@oregonstate.edu>
2025-03-04 13:45:36 -0800

    fix frame selection using popup, modify preprocessing ui slightly

--- src/ui/PreprocessingTab.cpp
+++ src/ui/PreprocessingTab.cpp
@@ -20,7 +20,7 @@ void PreprocessingTab::DisplayPreprocessingTab(bool& changed) {
 		ImGui::BeginChild("Controls", ImVec2(250, 0), true);
 
 		// Crop
-		ImGui::Text("Crop");
+		ImGui::SeparatorText("Crop");
 		static int crop = 60;
 		ImGui::SliderInt("Pixels", &crop, 0, 100);
 		if (ImGui::Button("Crop Bottom")) {
@@ -35,7 +35,7 @@ void PreprocessingTab::DisplayPreprocessingTab(bool& changed) {
 		if (ImGui::IsItemHovered()) ImGui::SetTooltip("Crops bottom of image (default: 60px for SEM infobar).");
 
 		// Stabilization
-		ImGui::Text("Stabilization");
+		ImGui::SeparatorText("Stabilization");
 		if (ImGui::Button("Stabilize")) {
 			std::vector<uint32_t*> frames;
 			utils::GetDataFromTextures(frames, m_processed_textures[0]->GetWidth(), m_processed_textures[0]->GetHeight(), m_processed_textures);
@@ -44,27 +44,54 @@ void PreprocessingTab::DisplayPreprocessingTab(bool& changed) {
 		}
 
 		// Frame Selection
-		ImGui::Text("Frame Selection");
-		if (ImGui::Button("Select All")) {
-			for (int i = 0; i < m_processed_textures.size(); i++) m_selected_textures_map[i] = 1;
-		}
-		ImGui::SameLine();
-		if (ImGui::Button("Deselect All")) m_selected_textures_map.clear();
-		if (ImGui::Button("Remove Selected")) {
-			changed = true;
-			std::vector<int> to_remove;
-			for (auto& item : m_selected_textures_map) to_remove.push_back(item.first);
-			std::sort(to_remove.begin(), to_remove.end());
-			for (int i = to_remove.size() - 1; i >= 0; i--) {
-				delete m_processed_textures[to_remove[i]];
-				m_processed_textures.erase(m_processed_textures.begin() + to_remove[i]);
-			}
-			m_selected_textures_map.clear();
-		}
+		ImGui::SeparatorText("Frame Selection");
 		// TODO: readd frame selection here
+		if (ImGui::Button("Choose Frames")) {
+			ImGui::OpenPopup("Choose Frames");
+		}
+
+		if (ImGui::BeginPopup("Choose Frames"))
+        {
+			if (ImGui::Button("Select All")) {
+				for (int i = 0; i < m_processed_textures.size(); i++) m_selected_textures_map[i] = 1;
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Deselect All")) m_selected_textures_map.clear();
+			if (ImGui::Button("Remove Selected")) {
+				changed = true;
+				std::vector<int> to_remove;
+				for (auto& item : m_selected_textures_map) to_remove.push_back(item.first);
+				std::sort(to_remove.begin(), to_remove.end());
+				for (int i = to_remove.size() - 1; i >= 0; i--) {
+					delete m_processed_textures[to_remove[i]];
+					m_processed_textures.erase(m_processed_textures.begin() + to_remove[i]);
+				}
+				m_selected_textures_map.clear();
+			}
+            ImGui::SeparatorText("Frames");
+            for (int i = 0; i < m_processed_textures.size(); i++) {
+				char name[100];
+				sprintf(name, "Image %d", i);
+				bool selected = m_selected_textures_map.find(i) != m_selected_textures_map.end();
+				if (selected) {
+					ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1, 0.2, 0.2, 1));
+				}
+				ImGui::ImageButton(name, (ImTextureID)m_processed_textures[i]->GetID(), ImVec2(100, 100));
+				if (selected) {
+					ImGui::PopStyleColor();
+				}
+				if (i % 6 != 5) ImGui::SameLine();
+				if (ImGui::IsItemHovered()) ImGui::SetTooltip("Frame %d", i);
+				if (ImGui::IsItemClicked()) {
+					if (selected) m_selected_textures_map.erase(i);
+					else m_selected_textures_map[i] = 1;
+				}
+			}
+            ImGui::EndPopup();
+        }
 
 		// Denoising
-		ImGui::Text("Denoising");
+		ImGui::SeparatorText("Denoising");
 		static int m_kernel_size = 3;
 		static float m_sigma = 1.0f;
 		ImGui::SliderInt("Kernel Size", &m_kernel_size, 1, 9, "%.0d");
@@ -77,7 +104,7 @@ void PreprocessingTab::DisplayPreprocessingTab(bool& changed) {
 			utils::LoadDataIntoTexturesAndFree(m_processed_textures, frames, m_processed_textures[0]->GetWidth(), m_processed_textures[0]->GetHeight());
 		}
 		static int tile_size = 256, overlap = 0, selected_model = 0;
-		const char* models[] = {"sfr_hrsem", "sfr_hrstem", "sfr_hrtem", "sfr_lrsem", "sfr_lrstem", "sfr_lrtem"};
+		static const char* models[] = {"sfr_hrsem", "sfr_hrstem", "sfr_hrtem", "sfr_lrsem", "sfr_lrstem", "sfr_lrtem"};
 		ImGui::Combo("Model", &selected_model, models, IM_ARRAYSIZE(models));
 		ImGui::SliderInt("Tile Size", &tile_size, 32, 512);
 		ImGui::SliderInt("Overlap", &overlap, 0, 256);
@@ -93,7 +120,7 @@ void PreprocessingTab::DisplayPreprocessingTab(bool& changed) {
 		if (!result) ImGui::TextColored(ImVec4(1, 0, 0, 1), "Model error!");
 
 		// Crack Detection
-		ImGui::Text("Crack Detection");
+		ImGui::SeparatorText("Crack Detection");
 		if (ImGui::Button("Detect Cracks")) {
 			std::vector<uint32_t*> frames;
 			utils::GetDataFromTextures(frames, m_processed_textures[0]->GetWidth(), m_processed_textures[0]->GetHeight(), m_processed_textures);
